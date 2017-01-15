66ac3f88425f78a3212ab87d9d55181d46b3e60c
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-01-14 18:55:31 -0800

    Connects posts index component to index action to fetch posts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-	return <div>List of Blog Posts</div>
-};
+class PostsIndex extends Component {
+	componentWillMount() {
+		this.props.fetchPosts();
+	}
+
+	render() {
+		return (
+			<div>List of blog posts</div>
+			);
+	}
+} 
+
+
+export default connect(null, { fetchPosts })(PostsIndex);
