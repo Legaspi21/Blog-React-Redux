aca859c005cf5b2670a56afa1b3eeccbc977390f
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-01-15 17:12:26 -0800

    Refactor posts new form and extracted code into helpers

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,7 +1,24 @@
+import _ from 'lodash';
 import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
+
+const FIELDS = {
+	title: {
+		type: 'input',
+		label: 'Title for Post'
+	},
+	categories: {
+		type: 'input',
+		label: 'Enter some categories for this post'
+	},
+	content: {  
+		type: 'textarea',
+		label: 'Post Contents'
+	}
+};
+// ['title', 'categories', 'content'];
 
 class PostsNew extends Component {
 	static contextTypes = {
@@ -18,35 +35,27 @@ class PostsNew extends Component {
 			});
 	}
 
+	renderField(fieldConfig, field) {
+		const fieldHelper = this.props.fields[field];
+		return (
+			<div key={fieldConfig.label.length} className={`form-group ${fieldHelper.touched && fieldHelper.invalid ? 'has-danger' : ''}`}>
+				<label>{fieldConfig.label}</label>
+				<fieldConfig.type type="text" className="form-control" {...fieldHelper}/>
+				<div className="text-help">
+					{fieldHelper.touched ? <p className='animated shake'>{fieldHelper.error}</p> : ''}
+				</div>
+			</div>
+		);
+	}
+
 	render() {
-		const { fields: { title, categories, content }, handleSubmit } = this.props;
+		const { handleSubmit } = this.props;
 		return (
 			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
 				<h3>Create A New Post</h3>
-
-					<div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
-						<label>Title</label>
-							<input type="text" className="form-control" {...title}/>
-							<div className="text-help">
-								{title.touched ? <p className='animated shake'>{title.error}</p> : ''}
-							</div>
-					</div>
-					<div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
-						<label>Categories</label>
-							<input type="text" className="form-control" {...categories}/>
-							<div className="text-help">
-								{categories.touched ? <p className='animated shake'>{categories.error}</p> : ''}
-							</div>
-					</div>
-					<div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
-						<label>Content</label>
-							<textarea className="form-control" {...content}/>
-							<div className="text-help">
-								{content.touched ? <p className='animated shake'>{content.error}</p> : ''}
-							</div>
-					</div>
-					<button type="submit" className="btn btn-primary">Submit</button>
-					<Link to="/" className="btn btn-danger">Cancel</Link>
+				{_.map(FIELDS, this.renderField.bind(this))}
+				<button type="submit" className="btn btn-primary">Submit</button>
+				<Link to="/" className="btn btn-danger">Cancel</Link>
 			</form>
 			)
 	}
@@ -55,15 +64,11 @@ class PostsNew extends Component {
 function validate(values) {
 	const errors = {};
 
-	if(!values.title) {
-		errors.title = 'Enter a title'
-	}
-	if(!values.categories) {
-		errors.categories = 'Enter a category'
-	}
-	if(!values.content) {
-		errors.content = 'Enter some content'
-	}
+	_.each(FIELDS, (type, field) => {
+		if (!values[field]) {
+			errors[field] = `Enter a ${field}`;
+		}
+	});
 
 	return errors;
 }
@@ -73,6 +78,6 @@ function validate(values) {
 
 export default reduxForm({ 
 	form: 'PostsNew',
-	fields: ['title', 'categories', 'content'],
+	fields: _.keys(FIELDS),
 	validate
 }, null, { createPost })(PostsNew);
--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,12 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsShow extends Component {
+	static contextTypes = {
+		router: PropTypes.object
+	}
+
 	componentWillMount() {
 		this.props.fetchPost(this.props.params.id)
 	}
+
+	onDeleteClick() {
+		this.props.deletePost(this.props.params.id)
+		.then(() => {
+				this.context.router.push('/');
+		});
+	}
+
 
 	render() {
 		const { post } = this.props;
@@ -16,6 +28,12 @@ class PostsShow extends Component {
 		}
 		return (
 			<div className="the-post">
+				<Link to="/">Back to index</Link>
+				<button 
+					className="btn btn-danger pull-xs-right"
+					onClick={this.onDeleteClick.bind(this)}>
+					Delete Post
+				</button>
 				<h3>{post.title}</h3>
 				<h6>Categories: {post.categories}</h6>
 				<p>{post.content}</p>
@@ -27,4 +45,4 @@ class PostsShow extends Component {
 function mapStateToProps(state) {
 	return { post: state.posts.post };
 }
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
